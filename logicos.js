377790c19702e42543a11aa85098b7284f601b9f
GloomyPepper7140 <gloomypepper714@gmail.com>
2022-10-20 10:53:07 -0300

    respostas

--- logicos.js
+++ logicos.js
@@ -9,7 +9,21 @@ let numero2 = "5";
 // "<=" é comparação se ele é menor ou igual ao valor;
 // "!=" é comparação se ele é diferente.
 
-
+//Respostas
 let resposta = numero != numero2;
+let resposta2 = numero <= numero2;
+let resposta3 = numero >= numero2;
+let resposta4 = numero == numero2;
+let resposta5 = numero === numero2;
+let resposta6 = numero < numero2;
+let resposta7 = numero > numero2;
 
+
+//console.log das respostas
 console.log (resposta)
+console.log (resposta2)
+console.log (resposta3)
+console.log (resposta4)
+console.log (resposta5)
+console.log (resposta6)
+console.log (resposta7)
