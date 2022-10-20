194f46c36e74be4543184dc7c0ce929c3c4cbc94
GloomyPepper7140 <gloomypepper714@gmail.com>
2022-10-20 11:04:33 -0300

    condicionais

---
+++ condicionais.js
@@ -0,0 +1,14 @@
+let nota1 = 6
+let nota2 = 5
+let nota3 = 7
+let nota4 = 10
+
+let media = (nota1+nota2+nota3)/3
+
+if (media <= 5){
+    console.log ('nota vermelha')
+} else if (media >= 6 && media <= 7){
+    console.log ('nota azul')
+} else if (media >= 8 && media <= 10){
+    console.log ('passou')
+} 
