65d383583b562c0e6b7387544988913a1d51a566
GloomyPepper7140 <gloomypepper714@gmail.com>
2022-10-20 11:05:23 -0300

    pontuacao

--- condicionais.js
+++ condicionais.js
@@ -1,9 +1,9 @@
-let nota1 = 6
-let nota2 = 5
-let nota3 = 7
-let nota4 = 10
+let nota1 = 6;
+let nota2 = 5;
+let nota3 = 7;
+let nota4 = 10;
 
-let media = (nota1+nota2+nota3)/3
+let media = (nota1+nota2+nota3)/3;
 
 if (media <= 5){
     console.log ('nota vermelha')
